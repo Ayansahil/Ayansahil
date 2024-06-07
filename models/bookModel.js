0a379d84a4f08f7fbb4504cc320e7303c43a3be9
ayan sahil <ayansahil@ayans-MacBook-Air.local>
2024-06-07 10:47:18 +0530

    first commit

---
+++ models/bookModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const bookModel = new mongoose.Schema({
+    poster: String,
+    name: String,
+    author: String,
+    isbn: String,
+    price: Number,
+    description: String,
+})
+
+const bookCollection = mongoose.model("book", bookModel);
+
+module.exports = bookCollection;
